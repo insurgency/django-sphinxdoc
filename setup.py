ad4b3439ff699a8b6ae0c52c44f11282997b5b06
Stefan Scherfke <stefan@sofa-rockers.org>
2010-08-19 21:47:45 +0200

    Workaround for some distutils-flaws

--- setup.py
+++ setup.py
@@ -4,13 +4,36 @@
 from distutils.core import setup
 
 
+class UltraMagicString(object):
+    # Catch-22:
+    # - if I return Unicode, python setup.py --long-description as well
+    #   as python setup.py upload fail with a UnicodeEncodeError
+    # - if I return UTF-8 string, python setup.py sdist register
+    #   fails with an UnicodeDecodeError
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __unicode__(self):
+        return self.value.decode('UTF-8')
+
+    def __add__(self, other):
+        return UltraMagicString(self.value + str(other))
+
+    def split(self, *args, **kw):
+        return self.value.split(*args, **kw)
+
+
 setup(
     name='django-sphinxdoc',
     version='0.3.2',
     author='Stefan Scherfke',
     author_email='stefan at sofa-rockers.org',
     description='Easily integrate Sphinx documentation into your website.',
-    # long_description=open('README.txt').read(),
+    long_description=UltraMagicString(open('README.txt').read()),
     url='http://stefan.sofa-rockers.org/django-sphinxdoc/',
     download_url='http://bitbucket.org/scherfke/django-sphinxdoc/downloads/',
     license='BSD',
