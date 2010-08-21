7ce70f29daaa8113741b75598c2d940b667f0dd6
Stefan Scherfke <stefan@sofa-rockers.org>
2010-08-21 23:40:11 +0200

    Started with a management command to update and build a project’s doc.

---
+++ sphinxdoc/management/commands/updatedoc.py
@@ -0,0 +1,49 @@
+import optparse
+import os.path
+import subprocess
+
+from django.core.management.base import BaseCommand, CommandError
+
+from sphinxdoc.models import Project, Document
+
+
+class Command(BaseCommand):
+    args = '<project_slug project_slug ...>'
+    help = ('Updates the documentation and the search index for the specified '
+            'projects.')    
+    option_list = BaseCommand.option_list + (
+        optparse.make_option('-b', '--build',
+            action='store_true',
+            dest='build',
+            default=False,
+            help='Run "sphinx-build" for each project before updating it.'),
+        optparse.make_option('--virtualenv',
+            dest='virtualenv',
+            default='',
+            help='Use this virtualenv to build project docs.'
+        )
+    )
+
+
+    def handle(self, *args, **options):
+        build = options['build']
+        virtualenv = options['virtualenv']
+        
+        for slug in args:
+            try:
+                project = Project.objects.get(slug=slug)
+            except Project.DoesNotExist:
+                raise CommandError('Project "%s" does not exist' % slug)
+
+            if build:
+                print 'Running "sphinx--build" for "%s" ...' % slug
+                cmd = 'sphinx-build'
+                if virtualenv:
+                    cmd = os.path.join(virtualenv, cmd)
+                # TODO: append args and path
+
+            print 'Importing JSON files for "%s" ...' % slug
+            
+            print 'Updating search index for "%s" ...' % slug
+            
+            print 'Done'
