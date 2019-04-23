ba207f0d061253e58f31827501ef8efa214c7dfb
Michel Sabchuk <michel@turbosys.com.br>
2019-04-23 10:26:55 -0300

    Refactor the settings to be compatible with django 1.11.

--- tests/settings.py
+++ tests/settings.py
@@ -31,12 +31,11 @@ TEMPLATES = [
     },
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.middleware.common.CommonMiddleware',
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
