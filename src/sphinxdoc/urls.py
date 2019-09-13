f39ac1edf7a62bd70b80e7fdc03736afccd34641
h1nk <12993584+h1nk@users.noreply.github.com>
2019-09-12 21:53:53 -0400

    Stop using old url() and use re_path() instead

--- src/sphinxdoc/urls.py
+++ src/sphinxdoc/urls.py
@@ -1,51 +1,19 @@
-"""
-URL conf for django-sphinxdoc.
+"""URL conf for django-sphinxdoc."""
 
-"""
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = [
-    url(
-        r'^$',
-        views.OverviewList.as_view(),
-        name='docs-list',
-    ),
-    url(
-        r'^(?P<slug>[\w-]+)/search/$',
-        views.ProjectSearchView(),
-        name='doc-search',
-    ),
+    re_path(r'^$', views.OverviewList.as_view(), name='docs-list'),
+    re_path(r'^(?P<slug>[\w-]+)/search/$', views.ProjectSearchView(), name='doc-search'),
     # These URLs have to be without the / at the end so that relative links in
     # static HTML files work correctly and that browsers know how to name files
     # for download
-    url(
-        (r'^(?P<slug>[\w-]+)/(?P<type_>_images|_static|_downloads|_source)/'
-         r'(?P<path>.+)$'),
-        views.sphinx_serve,
-    ),
-    url(
-        r'^(?P<slug>[\w-]+)/_objects/$',
-        views.objects_inventory,
-        name='objects-inv',
-    ),
-    url(
-        r'^(?P<slug>[\w-]+)/$',
-        views.documentation,
-        {'path': ''},
-        name='doc-index',
-    ),
-    url(
-        r'^(?P<slug>[\w-]+)/genindex/$',
-        views.documentation,
-        {'path': 'genindex'},
-        name='doc-genindex',
-    ),
-    url(
-        r'^(?P<slug>[\w-]+)/(?P<path>.+)/$',
-        views.documentation,
-        name='doc-detail',
-    ),
+    re_path(r'^(?P<slug>[\w-]+)/(?P<type_>_images|_static|_downloads|_source)/(?P<path>.+)$', views.sphinx_serve),
+    re_path(r'^(?P<slug>[\w-]+)/_objects/$', views.objects_inventory, name='objects-inv'),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.documentation, {'path': ''}, name='doc-index'),
+    re_path(r'^(?P<slug>[\w-]+)/genindex/$', views.documentation, {'path': 'genindex'}, name='doc-genindex'),
+    re_path(r'^(?P<slug>[\w-]+)/(?P<path>.+)/$', views.documentation, name='doc-detail'),
 ]
